1015ffb2bdaff56b5d8f816b2ea9ed40ccdd7010
Maksym Solomodenko <maxim.solomodenko@gmail.com>
2022-07-21 13:58:42 +0300

    regenerated grpc cpde

---
+++ protos/generate.sh
@@ -0,0 +1,19 @@
+#!bash/sh
+
+#backend
+cd protos 
+mkdir serverHandler
+protoc --go_out=serverHandler/ --go_opt=paths=source_relative \
+--go-grpc_out=serverHandle/ \
+ --go-grpc_opt=paths=source_relative \
+server_handler.proto
+
+#frontend
+curl -sSL https://github.com/grpc/grpc-web/releases/download/1.3.1/protoc-gen-grpc-web-1.3.1-linux-x86_64 \
+ /usr/local/bin/protoc-gen-grpc-web
+
+chmod +x /usr/local/bin/protoc-gen-grpc-web
+
+protoc -I . enviroment/protos/*.proto \
+--js_out=import_style=commonjs,binary:frontend/src/grpc \
+--grpc-web_out=import_style=commonjs,mode=grpcwebtext:frontend/src/grpc
